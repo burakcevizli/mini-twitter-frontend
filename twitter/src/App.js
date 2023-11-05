fd217dca79b655b90962c2227678066a528c9e5e
KrieqqeR <burakcevizli@gmail.com>
2023-11-05 22:58:20 +0300

    react rooter dom eklendi.

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Profile from './pages/Profile';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
   return (
+
     <div className="App">
-      <Profile />
+      <Switch>
+        <Route path="/profile" exact>
+          <Profile />
+        </Route>
+      </Switch>
     </div>
   );
 }
